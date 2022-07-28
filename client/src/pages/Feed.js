537ca4018c155d65084fae7090fbfa5c554260d6
gordo <rogelioalbertopedraza@gmail.com>
2022-07-28 09:36:52 -0500

    feed enhancements

--- client/src/pages/Feed.js
+++ client/src/pages/Feed.js
@@ -1,39 +1,42 @@
 import React from "react";
 
+import tweezer from "../assets/images/tweezer.svg"
+
 function Feed() {
     return (
       <div className="row min-vh-100">
-        <div className="col-2 border border-dark text-dark">
+        <div className="col-3 text-dark">
           <div className="container d-flex flex-column justify-content-center fs-3">
-            <div className="d-flex flex-row my-2">
+            <img src={tweezer} alt="tweezer logo" className="logo my-3"/>
+            <div className="option d-flex flex-row my-2">
               <i className="bi bi-house mx-2"></i>
               <p className="my-auto mx-3">Home</p>
             </div>
-            <div className="d-flex flex-row my-2">
+            <div className="option d-flex flex-row my-2">
               <i className="bi bi-search-heart mx-2"></i>
               <p className="my-auto mx-3">Explore</p>
             </div>
-            <div className="d-flex flex-row my-2">
+            <div className="option d-flex flex-row my-2">
               <i className="bi bi-bell mx-2"></i>
               <p className="my-auto mx-3">Notifications</p>
             </div>
-            <div className="d-flex flex-row my-2">
+            <div className="option d-flex flex-row my-2">
               <i className="bi bi-envelope mx-2"></i>
               <p className="my-auto mx-3">Messages</p>
             </div>
-            <div className="d-flex flex-row my-2">
+            <div className="option d-flex flex-row my-2">
               <i className="bi bi-bookmark mx-2"></i>
               <p className="my-auto mx-3">Bookmarks</p>
             </div>
-            <div className="d-flex flex-row my-2">
+            <div className="option d-flex flex-row my-2">
               <i className="bi bi-sticky mx-2"></i>
               <p className="my-auto mx-3">Lists</p>
             </div>
-            <div className="d-flex flex-row my-2">
+            <div className="option d-flex flex-row my-2">
               <i className="bi bi-person mx-2"></i>
               <p className="my-auto mx-3">Profile</p>
             </div>
-            <div className="d-flex flex-row my-2">
+            <div className="option d-flex flex-row my-2">
               <i className="bi bi-gear mx-2"></i>
               <p className="my-auto w-100 mx-3">More</p>
             </div>
@@ -42,8 +45,8 @@ function Feed() {
             </button>
           </div>
         </div>
-        <div className="col-8 border border-dark"></div>
-        <div className="col-2 border border-dark"></div>
+        <div className="col-6 border"></div>
+        <div className="col-3"></div>
       </div>
     );
 };
