f121830539563282dce2fe40527a78f4a1dba983
Chris <kimballc3@yahoo.com>
2022-07-28 14:47:29 -0500

    initial commit for graphQL queries

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,6 +1,43 @@
 import React from "react";
 
-function Signup() {
+// import { useMutation } from '@apollo/client';
+// import { ADD_USER } from '../utils/mutations';
+
+// import Auth from '../utils/auth';
+
+
+const Signup = () => {
+//   const [formState, setFormState] = useState({ username: '', email: '', password: '' });
+//   const [addUser, { error }] = useMutation(ADD_USER);
+
+//   // update state based on form input changes
+//   const handleChange = event => {
+//     const { name, value } = event.target;
+
+//     setFormState({
+//       ...formState,
+//       [name]: value
+//     });
+//   };
+
+//   // submit form
+// // submit form (notice the async!)
+// const handleFormSubmit = async event => {
+//   event.preventDefault();
+
+//   // use try/catch instead of promises to handle errors
+//   try {
+//     // execute addUser mutation and pass in variable data from form
+//     const { data } = await addUser({
+//       variables: { ...formState }
+//     });
+  
+//     Auth.login(data.addUser.token);
+//   } catch (e) {
+//     console.error(e);
+//   }
+// };
+
   return (
     <div className="row w-100 min-vh-100 bg-light">
       <div className="col-4 bg-img"></div>
