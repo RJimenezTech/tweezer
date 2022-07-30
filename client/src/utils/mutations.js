2e745dc9ef528bc58deccebd2e4c2e17a605c778
Chris <kimballc3@yahoo.com>
2022-07-30 00:12:56 -0500

    mutations added

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,69 +1,101 @@
 import { gql } from '@apollo/client';
 
-// export const LOGIN_USER = gql`
-//   mutation login($email: String!, $password: String!) {
-//     login(email: $email, password: $password) {
-//       token
-//       user {
-//         _id
-//         username
-//       }
-//     }
-//   }
-// `;
+export const LOGIN_USER = gql`
+  mutation Login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        name
+        username
+      }
+    }
+  }
+`;
 
-// export const ADD_USER = gql`
-//   mutation addUser($username: String!, $email: String!, $password: String!) {
-//     addUser(username: $username, email: $email, password: $password) {
-//       token
-//       user {
-//         _id
-//         username
-//       }
-//     }
-//   }
-// `;
+export const ADD_USER = gql`
+  mutation Login($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      _id
+      name
+      username
+      email
+    }
+  }
+`;
 
-// export const ADD_FRIEND = gql`
-//   mutation addFriend($id: ID!) {
-//     addFriend(friendId: $id) {
-//       _id
-//       username
-//       friendCount
-//       friends {
-//         _id
-//         username
-//       }
-//     }
-//   }
-// `;
+export const ADD_TWEET = gql`
+  mutation Tweet($userId: String!, $text: String!) {
+    addTweet(userId: $userId, text: $text) {
+      _id
+      text
+      createdAt
+      userId
+      replies {
+        _id
+        text
+        userId
+        replyCount
+      }
+      likes {
+        _id
+        name
+        username
+      }
+      retweets {
+        _id
+        name
+        username
+      }
+      replyCount
+      likesCount
+      retweetCount
+    }
+  }
+`;
 
-// export const ADD_THOUGHT = gql`
-//   mutation addThought($thoughtText: String!) {
-//     addThought(thoughtText: $thoughtText) {
-//       _id
-//       thoughtText
-//       createdAt
-//       username
-//       reactionCount
-//       reactions {
-//         _id
-//       }
-//     }
-//   }
-// `;
+// Follow
+export const FOLLOW_USER = gql`
+  mutation FollowUser($myId: String!, $otherId: String!) {
+    follow(myId: $myId, otherId: $otherId) {
+      _id
+      name
+      username
+      followerCount
+      followingCount
+    }
+  }
+`;
 
-// export const ADD_REACTION = gql`
-//   mutation addReaction($thoughtId: ID!, $reactionBody: String!) {
-//     addReaction(thoughtId: $thoughtId, reactionBody: $reactionBody) {
-//       _id
-//       reactionCount
-//       reactions {
-//         _id
-//         reactionBody
-//         createdAt
-//         username
-//       }
-//     }
-//   }
-// `;
+export const LIKE_TWEET = gql`
+  mutation LikeTweet($userId: String!, $tweetId: String!) {
+    likeTweet(userId: $userId, tweetId: $tweetId) {
+      _id
+      text
+      userId
+      likesCount
+      likes {
+        _id
+        name
+        username
+      }
+    }
+  }
+`;
+
+
+export const SHARE_TWEET = gql`
+  mutation Retweet($userId: String!, $tweetId: String!) {
+    retweet(userId: $userId, tweetId: $tweetId) {
+      _id
+      text
+      userId
+      retweets {
+        _id
+        name
+        username
+      }
+      retweetCount
+    }
+  }
+`;
