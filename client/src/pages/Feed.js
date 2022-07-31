105806fdf89405c0e9eaa27997ba4f9f2ab74dbc
realnifty <rogelioalbertopedraza@gmail.com>
2022-07-31 03:01:25 -0500

    moved Nav to component, setToggleTab functionality

--- client/src/pages/Feed.js
+++ client/src/pages/Feed.js
@@ -2,9 +2,8 @@ import React from "react";
 
 import HomeTweet from "../components/HomeTweet";
 import SingleTweet from "../components/SingleTweet";
+import Nav from "../components/Nav";
 import Auth from "../utils/auth";
-
-import tweezer from "../assets/images/tweezer.svg"
 
 function Feed() {
   const loggedIn = Auth.loggedIn();
@@ -13,44 +12,7 @@ function Feed() {
       {loggedIn ? (
         <div className="row bg-light w-100 min-vh-100">
           <div className="col-4 d-flex justify-content-end text-dark">
-            <div className="fs-3">
-              <img src={tweezer} alt="tweezer logo" className="logo my-3" />
-              <div className="option d-flex flex-row my-2">
-                <i className="bi bi-house mx-2"></i>
-                <p className="my-auto mx-3">Home</p>
-              </div>
-              <div className="option d-flex flex-row my-2">
-                <i className="bi bi-search-heart mx-2"></i>
-                <p className="my-auto mx-3">Explore</p>
-              </div>
-              <div className="option d-flex flex-row my-2">
-                <i className="bi bi-bell mx-2"></i>
-                <p className="my-auto mx-3">Notifications</p>
-              </div>
-              <div className="option d-flex flex-row my-2">
-                <i className="bi bi-envelope mx-2"></i>
-                <p className="my-auto mx-3">Messages</p>
-              </div>
-              <div className="option d-flex flex-row my-2">
-                <i className="bi bi-bookmark mx-2"></i>
-                <p className="my-auto mx-3">Bookmarks</p>
-              </div>
-              <div className="option d-flex flex-row my-2">
-                <i className="bi bi-sticky mx-2"></i>
-                <p className="my-auto mx-3">Lists</p>
-              </div>
-              <div className="option d-flex flex-row my-2">
-                <i className="bi bi-person mx-2"></i>
-                <p className="my-auto mx-3">Profile</p>
-              </div>
-              <div className="option d-flex flex-row my-2">
-                <i className="bi bi-gear mx-2"></i>
-                <p className="my-auto w-100 mx-3">More</p>
-              </div>
-              <button className="my-4 w-100 rounded-pill btn text-light fw-bold primary btn-lg">
-                Tweet
-              </button>
-            </div>
+            <Nav></Nav>
           </div>
           <div className="col-4 border center-feed">
             <header className="text-dark fs-5 fw-bold bg-light home-header">
