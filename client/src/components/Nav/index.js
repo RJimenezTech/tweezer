cbfdfde4b4424f43268677d9863b520aa8a140fe
realnifty <rogelioalbertopedraza@gmail.com>
2022-08-02 00:10:14 -0500

    minor fixes

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import {Modal, Button} from 'react-bootstrap';
+import {Modal, Button, Form} from 'react-bootstrap';
 
 import Auth from "../../utils/auth";
 import {useMutation} from '@apollo/client';
